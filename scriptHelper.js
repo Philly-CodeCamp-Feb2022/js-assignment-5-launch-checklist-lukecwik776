7a76919fb880189bff9ff98812bb612e35d744d9
lukecwik776 <102084230+lukecwik776@users.noreply.github.com>
2022-04-11 01:22:16 -0500

    Made changes to formSubmission function to include a conditional that validates all user inputs and relay the launch status to the user and if status is not ready includes the reason why

--- scriptHelper.js
+++ scriptHelper.js
@@ -27,6 +27,65 @@ function validateInput(testInput) {
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+    let pilotReadyForLaunch = false;
+    let copilotReadyForLaunch = false;
+    let fuelReadyForLaunch = false;
+    let cargoReadyForLaunch = false;
+
+
+    if (validateInput(pilot.value) === "Not a Number") {
+        document.getElementById("pilotStatus").innerHTML = `Pilot ${pilot.value} is ready for launch.`;
+        pilotReadyForLaunch = true;
+    } else {
+        document.getElementById("pilotStatus").innerHTML = `${pilot} is not an acceptable entry.`;
+        alert(`${pilot.value} is not an acceptable entry for Pilot Name`);
+        event.preventDefault();
+    }
+    if (validateInput(copilot.value) === "Not a Number") {
+        document.getElementById("copilotStatus").innerHTML = `Co-pilot ${copilot.value} is ready for launch.`;
+        copilotReadyForLaunch = true;
+    } else {
+        document.getElementById("copilotStatus").innerHTML = `${copilot} is not an acceptable entry.`;
+        alert(`${copilot.value} is not an acceptable entry for Co-Pilot Name`);
+        event.preventDefault();
+    }
+    if (validateInput(fuelLevel.value) === "Is a Number") {
+        if (Number(fuelLevel.value) >= 10000) {
+            document.getElementById("fuelStatus").innerHTML = `Fuel Level is ${fuelLevel.value}(L). Ready for launch.`;
+            fuelReadyForLaunch = true;
+        } else {
+            document.getElementById("fuelStatus").innerHTML = `Fuel Level is ${fuelLevel.value}(L). Fuel level too low. Not enough fuel for journey`;
+            document.getElementById("launchStatus").innerHTML = "Shuttle Not Ready For Launch";
+            document.getElementById("launchStatus").style.color = "red";
+            list.style.visibility = "visible";
+            event.preventDefault();
+        }
+    } else {
+        document.getElementById("fuelStatus").innerHTML = `${fuelLevel.value} is not an acceptable entry.`;
+        alert(`${fuelLevel.value} is not an acceptable entry for Fuel Level`);
+        event.preventDefault();
+    }
+    if (validateInput(cargoLevel.value) === "Is a Number") {
+        if (Number(cargoLevel.value) <= 10000) {
+            document.getElementById("cargoStatus").innerHTML = `Cargo Mass is ${cargoLevel.value}(kg). Ready for launch.`;
+            cargoReadyForLaunch = true;
+        } else {
+            document.getElementById("cargoStatus").innerHTML = `Cargo Mass is ${cargoLevel.value}(kg). Cargo Mass is to large for shuttle takeoff.`;
+            document.getElementById("launchStatus").innerHTML = "Shuttle Not Ready For Launch";
+            document.getElementById("launchStatus").style.color = "red";
+            list.style.visibility = "visible";
+            event.preventDefault();
+        }
+    } else {
+        document.getElementById("cargoStatus").innerHTML = `${cargoLevel.value} is not an acceptable entry.`;
+        alert(`${cargoLevel.value} is not an acceptable entry for Cargo Mass`);
+        event.preventDefault();
+    }
+    if (pilotReadyForLaunch === true && copilotReadyForLaunch === true && fuelReadyForLaunch === true && cargoReadyForLaunch === true) {
+        document.getElementById("launchStatus").innerHTML = "Shuttle Is Ready For Launch";
+        document.getElementById("launchStatus").style.color = "green";
+        event.preventDefault();
+    }
 
 }
 
