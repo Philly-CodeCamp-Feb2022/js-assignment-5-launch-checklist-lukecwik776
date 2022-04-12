b21c145ff401e305abac18bceef298c77a5a7a14
lukecwik776 <102084230+lukecwik776@users.noreply.github.com>
2022-04-11 19:07:49 -0500

    script.js modified to assign myFetch to the listedPlanetsResponse variable. Called functions from scriptHelper pickPlanet and addDestinationInfo. Variables created for the chosen planets key/value pairs for use in the addDestinationInfo function

--- script.js
+++ script.js
@@ -19,13 +19,22 @@ window.addEventListener("load", function () {
 
     let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse;
+    let listedPlanetsResponse = myFetch();
     listedPlanetsResponse.then(function (result) {
         listedPlanets = result;
         console.log(listedPlanets);
     }).then(function () {
         console.log(listedPlanets);
         // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+        let chosenPlanet = pickPlanet(listedPlanets);
+        let planetName = chosenPlanet.name;
+        let planetDiameter = chosenPlanet.diameter;
+        let planetDistance = chosenPlanet.distance;
+        let planetMoons = chosenPlanet.moons;
+        let planetStar = chosenPlanet.star;
+        let planetImage = chosenPlanet.image;
+
+        addDestinationInfo(document, planetName, planetDiameter, planetStar, planetDistance, planetMoons, planetImage)
     })
 
 });
